735668cfa8c050acf42a344983cb1c4f0c959368
arebecchi <arebecchi@devartis.com>
2015-02-10 10:36:09 -0300

    l1 chrom invariant

--- aux.py
+++ aux.py
@@ -1,22 +1,17 @@
 import numpy as np
 import cv2
-name = 'auto'
-if
-img = cv2.imread('img/'+name+'.png')
+name = 'balcon'
+ext = 'png'
+#img = cv2.imread('img/'+name+'.png')
+img = cv2.imread('img/out/'+name+'/out2_151.png')
 cv2.namedWindow('Original image', cv2.WINDOW_NORMAL)
 cv2.imshow('Original image', img)
 
-sp = 15
-sr = 30
-mshft2=cv2.pyrMeanShiftFiltering(img,sp,sr)
+min_angle = 158
 
-cv2.namedWindow('Mean Shifted 2', cv2.WINDOW_NORMAL)
-cv2.imshow('Mean Shifted 2', mshft2)
-cv2.imwrite('img/out/'+name+'/mshft2.png', mshft2)
-
-tmin2 = 150
-tmax2 = 250
-edges2 = cv2.Canny(mshft2,tmin2,tmax2 )
+tmin2 = 20
+tmax2 = 55
+edges2 = cv2.Canny(img,tmin2,tmax2 )
 
 #PRINT edges 2
 cv2.namedWindow('Edges2', cv2.WINDOW_NORMAL)
--- shadows.py
+++ shadows.py
@@ -4,23 +4,208 @@ import math
 import matplotlib
 import matplotlib.pyplot as plt
 
+def get_U():
+    return np.matrix([[1.0/math.sqrt(2), -1.0/math.sqrt(2), 0], [1.0/math.sqrt(6), 1.0/math.sqrt(6), -2.0/math.sqrt(6)]])
 
+def project_to_2d(log_chrom):
+    U = get_U()
+    return [[np.array((U * np.matrix(elem).transpose())) for elem in row] for row in log_chrom]
+
+
+def minimize_entropy(two_dim):
+    # FIND MIN ANGLE
+    min_entropy = 0
+    min_mono = []
+    entropy_array = []
+
+    angles = [158]#xrange(1, 180, 3)
+    min_angle = 0   #hack horriible
+
+    for angle in angles:
+        print str(angle)
+        rad = math.radians(angle)
+        mono = [[elem[0]*math.cos(rad)+elem[1]*math.sin(rad) for elem in row] for row in two_dim]
+        ent = entropy(mono)
+        entropy_array.append(ent)
+        if min_angle == 0 or ent < min_entropy:
+            min_entropy = ent
+            min_mono = mono
+            min_angle = angle
+    return (min_mono, min_angle)
+
+
+def show_image(name, img):
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.imshow(name, img)
+
+def show_and_save(param, name, ext, img, fact=1):
+    cv2.namedWindow(param, cv2.WINDOW_NORMAL)
+    cv2.imshow(param, img)
+    cv2.imwrite('img/out/'+name+'/out_'+param+'.'+ext, img*fact)
+
+
+def adapt_to_range(min_mono):
+
+    #search for max min values
+    mono_img = np.array(min_mono)
+    min_val = mono_img.min()
+    max_val = mono_img.max()
+
+    print "\n>>>Invariant image<<<"
+    print "Min val: "+str(min_val)
+    print "Max val: "+str(max_val)
+
+    rango = max_val-min_val
+    coef = 1 / rango
+
+    print "Range: "+str(rango)
+    print "Coefficient: "+str(coef)
+
+    #ADAPT RANGE TO 0..1----------------
+    min_mono2 = np.array([[coef*(elem-min_val) for elem in row] for row in min_mono])
+    return min_mono2
+
+
+def get_invariant_l1_chrom_image(two_dim, min_angle):
+    U = get_U()
+    rad = math.radians(min_angle)
+    e_orth = np.array([math.cos(rad), math.sin(rad)])
+    P_e_orth = np.matrix([[e_orth[0] * e_orth[0],e_orth[0] * e_orth[0]],[e_orth[0] * e_orth[1],e_orth[1] * e_orth[1]]])
+    X_tita = [[P_e_orth * np.matrix(elem) for elem in row] for row in two_dim]
+    p_monio = [[(U.transpose() * np.matrix(elem)) for elem in row] for row in X_tita]
+    show_image("pmonio",np.array(p_monio))
+    c_monio = np.array([[np.array([math.exp(val) for val in elem]) for elem in row] for row in p_monio])
+    (log_chrom, L1chrom) = log_chromaticity_image(c_monio)
+    return L1chrom
 def listener():
     # Load an color image in grayscale
-
-    # # TODO: que se haga una sola vez
-    # u = np.array([1/math.sqrt(3)] * 3)
-    # ut = np.array([1/math.sqrt(3)] * 3)
-    # ut.transpose()
-    # I = np.identity(3)
-    # P = I - u * ut
-    U = np.array([[1.0/math.sqrt(2), -1.0/math.sqrt(2), 0], [1.0/math.sqrt(6), 1.0/math.sqrt(6), -2.0/math.sqrt(6)]])
 
     name = 'balcon'
     ext = 'png'
     img = cv2.imread('img/'+name+"."+ext)
-    cv2.namedWindow('Original image', cv2.WINDOW_NORMAL)
-    cv2.imshow('Original image', img)
+    show_image('Original image', img)
+
+    (log_chrom, L1chrom) = log_chromaticity_image(img)
+
+    lc = np.array(log_chrom)
+    l1cp = np.array(L1chrom)
+
+    print("max---"+str(l1cp.max()))
+    print("min---"+str(l1cp.min()))
+    show_and_save('L1_Chromacity', name, ext, l1cp, 255)
+
+    two_dim = project_to_2d(log_chrom)
+
+    (min_mono, min_angle) = minimize_entropy(two_dim)
+
+    print 'min angle: '+str(min_angle)
+
+    log_chrom_inv = get_invariant_l1_chrom_image(two_dim,min_angle)
+    lci = np.array(log_chrom_inv)
+    show_and_save('L1_Chromacity_invariant', name, ext, lci)
+    #matplotlib.rcParams['axes.unicode_minus'] = False
+    #fig, ax = plt.subplots()
+    #ax.plot(angles, entropy_array, '.')
+    #ax.set_title('Entropy')
+    #plt.show()
+    #plt.savefig('img/out/'+name+'/plot.png')
+
+    #print & show image 1
+    #show_and_save('mono', name, ext, mono_img)
+    # min_mono2 = adapt_to_range(min_mono)
+    #
+    # print "\n>>>Invariant image(in range)<<<"
+    # min_val = min_mono2.min()
+    # max_val = min_mono2.max()
+    #
+    # print "Min val: "+str(min_val)
+    # print "Max val: "+str(max_val)
+    #
+    # #PRINT IMAGE 2
+    # show_and_save('mono2', name, ext, min_mono2, 255)
+    #
+    # #-------------------------------------------
+    # #EDGES CANNY------------------
+    # sp = 15
+    # sr = 30
+    # r = np.uint8(min_mono2*255)
+    # m = cv2.merge((r,r,r))
+    # mshft=cv2.pyrMeanShiftFiltering(m,sp,sr)
+    # (r,g,b) = cv2.split(mshft)
+    #
+    # cv2.namedWindow('Mean Shifted', cv2.WINDOW_NORMAL)
+    # cv2.imshow('Mean Shifted', r)
+    # cv2.imwrite('img/out/'+name+'/mshft_'+str(min_angle)+'.'+ext, mshft)
+    #
+    # mshft = r
+    # tmin = 20
+    # tmax = 55
+    # edges1 = cv2.Canny(min_mono2,tmin,tmax)
+    #
+    # #PRINT edges
+    # cv2.namedWindow('Edges1', cv2.WINDOW_NORMAL)
+    # cv2.imshow('Edges1', edges1)
+    # cv2.imwrite('img/out/'+name+'/edges_'+str(min_angle)+'.'+ext, edges1)
+    # #-------------------------------------------------
+    #
+    # #MEAN SHIFTED---------------------
+    # sp = 25
+    # sr = 50
+    # mshft2=cv2.pyrMeanShiftFiltering(img,sp,sr)
+    #
+    # cv2.namedWindow('Mean Shifted 2', cv2.WINDOW_NORMAL)
+    # cv2.imshow('Mean Shifted 2', mshft2)
+    # cv2.imwrite('img/out/'+name+'/mshft2_'+str(min_angle)+'.'+ext, mshft2)
+    #
+    # tmin2 = 150
+    # tmax2 = 250
+    # edges2 = cv2.Canny(mshft2,tmin2,tmax2 )
+    #
+    # #PRINT edges 2
+    # cv2.namedWindow('Edges2', cv2.WINDOW_NORMAL)
+    # cv2.imshow('Edges2', edges2)
+    # cv2.imwrite('img/out/'+name+'/edges2_'+str(min_angle)+'.'+ext, edges2)
+    # #-------------------------------------------
+    # #DIFERENCE---------------------
+    # diff = edges2-edges1
+    # cv2.namedWindow('Diff', cv2.WINDOW_NORMAL)
+    # cv2.imshow('Diff', diff)
+    # cv2.imwrite('img/out/'+name+'/diff_'+str(min_angle)+'.'+ext, diff)
+    #-------------------------------------------
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def entropy(matrix):
+    mono_img = np.array(matrix)
+    min_val = mono_img.min()
+    max_val = mono_img.max()
+    rango = max_val-min_val
+    coef = 1 / rango
+
+    bins = 64
+    sums = [0] * bins
+
+    for row in matrix:
+        for val in row:
+            normal_val = coef*(val-min_val)
+            bin = int(normal_val*bins)
+            if bin == 64:
+                bin = 63
+            sums[bin] += 1
+    ent = 0
+    for bin in xrange(bins):
+        val = sums[bin]
+        try:
+            ent += val * math.log(val)
+        except ValueError:
+            ent += val * math.log(0.0001)
+    return -ent
+
+
+def mono_image(width, height):
+    return [[np.int64(0) for j in xrange(width)] for j in xrange(height)]
+
+def log_chromaticity_image(img):
 
     height = img.shape[0]
     width = img.shape[1]
@@ -57,162 +242,7 @@ def listener():
                 L1chrom[j][i][1] = c_2 / (c_1 + c_2 + c_3)
                 L1chrom[j][i][2] = c_3 / (c_1 + c_2 + c_3)
 
-    l1cp = np.array(L1chrom)
-    print("max---"+str(l1cp.max()))
-    print("min---"+str(l1cp.min()))
-    cv2.namedWindow('L1 Chromacity', cv2.WINDOW_NORMAL)
-    cv2.imshow('L1 Chromacity', l1cp)
-    cv2.imwrite('img/out/'+name+'/out_l1_chromacity.'+ext, l1cp*255)
-
-    #show log chromacity image
-    lc = np.array(log_chrom)
-    min_val = lc.min()
-    max_val = lc.max()
-
-    two_dim = [[np.array((U * np.matrix(elem).transpose())) for elem in row] for row in log_chrom]
-
-    # FIND MIN ANGLE
-    min_entropy = 0
-    min_mono = []
-    entropy_array = []
-
-    angles = [151]#xrange(1, 180, 3)
-    min_angle = 0   #hack horriible
-
-    for angle in angles:
-        print str(angle)
-        rad = math.radians(angle)
-        mono = [[elem[0]*math.cos(rad)+elem[1]*math.sin(rad) for elem in row] for row in two_dim]
-        ent = entropy(mono)
-        entropy_array.append(ent)
-        if min_angle == 0 or ent < min_entropy:
-            min_entropy = ent
-            min_mono = mono
-            min_angle = angle
-
-    print 'min angle: '+str(min_angle)
-
-    #matplotlib.rcParams['axes.unicode_minus'] = False
-    #fig, ax = plt.subplots()
-    #ax.plot(angles, entropy_array, '.')
-    #ax.set_title('Entropy')
-    #plt.show()
-    #plt.savefig('img/out/'+name+'/plot.png')
-
-    #search for max min values
-    mono_img = np.array(min_mono)
-    min_val = mono_img.min()
-    max_val = mono_img.max()
-
-    print "\n>>>Invariant image<<<"
-    print "Min val: "+str(min_val)
-    print "Max val: "+str(max_val)
-
-    #print & show image 1
-    #cv2.namedWindow('Mono', cv2.WINDOW_NORMAL)
-    #cv2.imshow('Mono', mono_img)
-    #cv2.imwrite('img/out/'+name+'/out_'+str(min_angle)+'.'+ext, mono_img)
-
-    rango = max_val-min_val
-    coef = 1 / rango
-
-    print "Range: "+str(rango)
-    print "Coefficient: "+str(coef)
-
-    #ADAPT RANGE TO 0..1----------------
-    min_mono2 = np.array([[coef*(elem-min_val) for elem in row] for row in min_mono])
-
-    print "\n>>>Invariant image(in range)<<<"
-    min_val = min_mono2.min()
-    max_val = min_mono2.max()
-
-    print "Min val: "+str(min_val)
-    print "Max val: "+str(max_val)
-
-    #PRINT IMAGE 2
-    cv2.namedWindow('Mono2', cv2.WINDOW_NORMAL)
-    cv2.imshow('Mono2', min_mono2)
-    cv2.imwrite('img/out/'+name+'/out2_'+str(min_angle)+'.'+ext, min_mono2*255)
-
-    #-------------------------------------------
-    #EDGES CANNY------------------
-    sp = 15
-    sr = 30
-    r = np.uint8(min_mono2*255)
-    m = cv2.merge((r,r,r))
-    mshft=cv2.pyrMeanShiftFiltering(m,sp,sr)
-    (r,g,b) = cv2.split(mshft)
-
-    cv2.namedWindow('Mean Shifted', cv2.WINDOW_NORMAL)
-    cv2.imshow('Mean Shifted', r)
-    cv2.imwrite('img/out/'+name+'/mshft_'+str(min_angle)+'.'+ext, mshft)
-
-    tmin = 100
-    tmax = 150
-    edges1 = cv2.Canny(mshft,tmin,tmax)
-
-    #PRINT edges
-    cv2.namedWindow('Edges1', cv2.WINDOW_NORMAL)
-    cv2.imshow('Edges1', edges1)
-    cv2.imwrite('img/out/'+name+'/edges_'+str(min_angle)+'.'+ext, edges1)
-    #-------------------------------------------------
-
-    #MEAN SHIFTED---------------------
-    sp = 15
-    sr = 30
-    mshft2=cv2.pyrMeanShiftFiltering(img,sp,sr)
-
-    cv2.namedWindow('Mean Shifted 2', cv2.WINDOW_NORMAL)
-    cv2.imshow('Mean Shifted 2', mshft2)
-    cv2.imwrite('img/out/'+name+'/mshft2_'+str(min_angle)+'.'+ext, mshft2)
-
-    tmin2 = 150
-    tmax2 = 250
-    edges2 = cv2.Canny(mshft2,tmin2,tmax2 )
-
-    #PRINT edges 2
-    cv2.namedWindow('Edges2', cv2.WINDOW_NORMAL)
-    cv2.imshow('Edges2', edges2)
-    cv2.imwrite('img/out/'+name+'/edges2_'+str(min_angle)+'.'+ext, edges2)
-    #-------------------------------------------
-    #DIFERENCE---------------------
-    diff = edges2-edges1
-    cv2.namedWindow('Diff', cv2.WINDOW_NORMAL)
-    cv2.imshow('Diff', diff)
-    cv2.imwrite('img/out/'+name+'/diff_'+str(min_angle)+'.'+ext, diff)
-    #-------------------------------------------
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def entropy(matrix):
-    mono_img = np.array(matrix)
-    min_val = mono_img.min()
-    max_val = mono_img.max()
-    rango = max_val-min_val
-    coef = 1 / rango
-
-    bins = 64
-    sums = [0] * bins
-
-    for row in matrix:
-        for val in row:
-            normal_val = coef*(val-min_val)
-            bin = int(normal_val*bins)
-            if bin == 64:
-                bin = 63
-            sums[bin] += 1
-    ent = 0
-    for bin in xrange(bins):
-        val = sums[bin]
-        try:
-            ent += val * math.log(val)
-        except ValueError:
-            ent += val * math.log(0.0001)
-    return -ent
-
-
-def mono_image(width, height):
-    return [[np.int64(0) for j in xrange(width)] for j in xrange(height)]
+    return (log_chrom, L1chrom)
 
 if __name__ == '__main__':
     listener()
